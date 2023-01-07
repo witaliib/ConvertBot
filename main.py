2578ce173569443100eb710377654e111f53eadc
witaliib <50584429+witaliib@users.noreply.github.com>
2023-01-08 00:11:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import telebot
+from extentions import APIException, Convertor
+from config import TOKEN, exchanges
+import traceback
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = "Этот бот конвертирует валюты \nдля того чтобы увидеть список доступных валют нажмите /values"
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    values = message.text.split(' ')
+    try:
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров!')
+
+        answer = Convertor.get_price(*values)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка в команде:\n{e}")
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        bot.reply_to(message, f"Неизвестная ошибка:\n{e}")
+    else:
+        bot.reply_to(message, answer)
+
+
+bot.polling()
